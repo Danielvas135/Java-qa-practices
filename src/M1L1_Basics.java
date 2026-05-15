f9d4f76085e5f9e30c33504a1d1e8dc6ea5a3c90
Danielvas135 <danielvas135@gmail.com>
2026-05-15 10:20:01 -0700

    Initial commit - M1L1 Basics and project setup

---
+++ src/M1L1_Basics.java
@@ -0,0 +1,19 @@
+public class M1L1_Basics {
+
+    public static void main(String[] args) {
+        System.out.println("=== M1-L1 Java Basics - Working ===");
+
+        int attackLevel = 88;
+        int targetAttack = 92;
+        double xpNeeded = 2131477.0;
+        boolean isGrinding = true;
+        String playerName = "ShibaScylla2";
+
+        System.out.println("Player: " + playerName);
+        System.out.println("Current Attack: " + attackLevel);
+        System.out.println("Target Attack: " + targetAttack);
+        System.out.println("XP needed: " + xpNeeded);
+        System.out.println("Grinding today? " + isGrinding);
+    }
+
+}
