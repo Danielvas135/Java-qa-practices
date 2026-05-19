c6cb42d5570e72bf64899a63c97c9e73d405f0fc
Danielvas135 <danielvas135@gmail.com>
2026-05-19 16:28:18 -0700

    M1L4_Loops worked on learning about for loops

---
+++ src/M1L4_Loops.java
@@ -0,0 +1,35 @@
+public class M1L4_Loops {
+    public static void main(String[] args){
+
+        System.out.println("=== M1-L4 Java Loops ===\n");
+
+        String champion = "Neeko";
+
+        //1, For Loop - Most common in automation
+        System.out.println("Neeko is leveling up:");
+        for(int level = 1; level <=18; level++){
+            System.out.println("Level " + level);
+        }
+        System.out.println("\n--- Spell Rotation (While Loop) ---");
+        //2. While Loop
+        int spellCooldown = 7;
+        while(spellCooldown > 0) {
+            System.out.println("Blooming Burst... Ready in: " + spellCooldown);
+            spellCooldown--;
+        }
+                System.out.println("Blooming Burst is ready!");
+
+
+        System.out.println("\n--- Checking Items in Shop (For-each style) ---");
+
+        // 3. For-each loop (very useful)
+        String[] items = {"Rocketbelt", "Shadowflame", "Zhonyas", "Rabadons"};
+
+        for (String item : items) {
+            System.out.println("Neeko bought: " + item);
+        }
+
+        System.out.println("\nNeeko finished her shopping spree!");
+    }
+
+}
